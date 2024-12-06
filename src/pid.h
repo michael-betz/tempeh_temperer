6cdc75c9a98b11fda5e29afa374f267d67408132
Michael Betz <michael.betz@terapet.ch>
2024-12-06 16:38:25 +0100

    some cleanup

--- src/pid.h
+++ src/pid.h
@@ -1,21 +1,10 @@
 #pragma once
 
-// Number of fracional bits
-// Need high resolution for integral-term to accumulate
-// small differences with low (0.1) coefficients
-#define FP_FRAC 6
-
-#define FP_SCALE (1 << FP_FRAC)
-#define FP_ROUND (1 << (FP_FRAC - 1))
-
-// Convert floating point constant to fixed point
-#define FP(v) ((int32_t)(v * FP_SCALE + 0.5))
-
 // How many temperature values to average [cycles]
 #define N_AVG 4
 
 // ---------------------------------------------------------------
-// Inner loop which controls the heater PWM from air temperature
+//  Inner loop which controls the heater PWM from air temperature
 // ---------------------------------------------------------------
 // Used in dual sensor mode
 #define AIR_KP_DUAL FP(200.0)  // PWM units / degC
@@ -26,8 +15,8 @@
 #define AIR_KI_SINGLE FP(0.5)
 
 // PWM-value limits. Valid range from 0 to 0xFF
-#define POWER_MAX_LIMIT (0xFF << FP_FRAC)
-#define POWER_MIN_LIMIT (4 << FP_FRAC)  // not 0 to keep powerbank from shutting down
+#define POWER_MAX_LIMIT FP(0xFF)
+#define POWER_MIN_LIMIT FP(0x04)  // not 0 to keep powerbank from shutting down
 
 // ---------------------------------------------------------------
 //  Outer loop which controls Tempeh probe temperature
@@ -37,6 +26,21 @@
 // Air temperature set-point limits in [degC]
 #define AIR_MAX_LIMIT FP(38.0)
 #define AIR_MIN_LIMIT FP(20.0)
+
+// ---------------------------------------------------------------
+//  Fixed point integer helpers
+// ---------------------------------------------------------------
+// Number of fracional bits for PID stuff
+// Need high resolution for integral-term to accumulate
+// small differences with low (0.1) coefficients
+#define FP_FRAC 6
+
+#define FP_SCALE (1 << FP_FRAC)
+#define FP_ROUND (1 << (FP_FRAC - 1))
+
+// Convert floating point constant to fixed point
+#define FP(v) ((int32_t)(v * FP_SCALE + 0.5))
+
 
 extern int16_t measured_air_temperature;
 extern int16_t measured_probe_temperature;
@@ -69,5 +73,3 @@ bool load_ee(int32_t *val, uint8_t slot);
 
 // Limit a value to [a, b]
 int32_t limit(int32_t val, int32_t a, int32_t b);
-
-
